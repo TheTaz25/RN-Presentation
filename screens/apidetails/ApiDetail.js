c7ef227b936749574dcb908e328b765457314da2
TheTaz <thetazlp@hotmail.de>
2019-04-24 19:36:25 +0200

    Edit ApiDetails (INIT)

---
+++ screens/apidetails/ApiDetail.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ApiDetails extends React.Component {
+  state = {
+
+  }
+}
+
+export default ApiDetails;
---
+++ screens/apidetails/ios/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ApiDetail from '../ApiDetail.js';
+
+import { Container, Content } from 'native-base';
+
+class IosApiDetail extends ApiDetail {
+  render(){
+    return null;
+  }
+}
+
+export default IosApiDetail;
